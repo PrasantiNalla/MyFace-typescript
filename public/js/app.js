66eaa2329a1c88e470ded26b840b55e7c8bfb576
Prasanti <nalla.prasanti@gmail.com>
2023-01-31 16:52:11 +0000

    added menu to header

--- public/js/app.js
+++ public/js/app.js
@@ -3,17 +3,15 @@ document.addEventListener("DOMContentLoaded", function () {
   const mybutton = document.getElementById("my-button");
 
   const postColor = document.getElementsByClassName("eachPost");
-  console.log(postColor)
-  //postColor.forEach(post => { post.style.backgroundColor = `rgb(${Math.random() * 255}, ${Math.random() * 255}, ${Math.random() * 255})` })
-
   mybutton.addEventListener("click", function () {
     for (let i = 0; i < postColor.length; i++) {
       postColor[i].style.backgroundColor = `rgb(${Math.random() * 255}, ${Math.random() * 255}, ${Math.random() * 255})`
     }
   });
 
-  const menuIcon = document.getElementById("menu-icon");
 
+  // menu icon event listener
+  const menuIcon = document.getElementById("menu-icon");
   menuIcon.addEventListener("click", function () {
     const menu = document.getElementById("menu");
     if (menu.style.display === "none") {
@@ -22,15 +20,5 @@ document.addEventListener("DOMContentLoaded", function () {
       menu.style.display = "none";
     }
   });
-
-
-  // function myFunction() {
-  //   var x = document.getElementById("myDIV");
-  //   if (x.style.display === "none") {
-  //     x.style.display = "block";
-  //   } else {
-  //     x.style.display = "none";
-  //   }
-  // }
 
 });
